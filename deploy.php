cbd8ca26f144012571e90cb85526d93cc1f6d297
冯誉信 <ninvfeng@qq.com>
2018-04-23 14:17:28 +0800

    test

--- deploy.php
+++ deploy.php
@@ -1,20 +1,20 @@
 <?php
 //github webhook php部署脚本
 
-//判断签名是否存在
-if(!$_SERVER['HTTP_X_HUB_SIGNATURE']){
-	echo "签名不存在";
-	exit();
-}
+// //判断签名是否存在
+// if(!$_SERVER['HTTP_X_HUB_SIGNATURE']){
+// 	echo "签名不存在";
+// 	exit();
+// }
 
-//验证签名
-$token=include "token.php";
-$data=file_get_contents('php://input');
-list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
-if($hash!==hash_hmac($algo, $data, $token)){
-	echo "签名错误";
-	exit();
-}
+// //验证签名
+// $token=include "token.php";
+// $data=file_get_contents('php://input');
+// list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
+// if($hash!==hash_hmac($algo, $data, $token)){
+// 	echo "签名错误";
+// 	exit();
+// }
 
 //验证部署目录
 if(!is_dir($_GET['path'])){
