266b10f457a077bdaa10300ae824044a6a18e232
冯誉信 <ninvfeng@qq.com>
2018-04-23 10:51:43 +0800

    test

--- deploy.php
+++ deploy.php
@@ -4,9 +4,6 @@ if($_GET['token']=='ninvfeng'){
 	$dir=$_GET['path'];
 	if(is_dir($dir)){
 		$cmd="cd ".$dir." && git checkout . && git pull --force";
-		echo $cmd;
-		echo "<br>";
-		exec($cmd,$res);
 		print_r($res);
 	}else{
 		echo "dir not exist";
